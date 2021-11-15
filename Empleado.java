dcb93a53ce3bfcd7d75b8f01ac15e9ffb66ddbd3
Mariela Amanta <mamanta4438@uta.edu.ec>
2021-11-15 18:27:29 -0500

    Clase empleado añadido

---
+++ Empleado.java
@@ -0,0 +1,10 @@
+public class Empleado(){
+   
+               private string cedula;
+               private String nombre;
+
+               public String getCedula(){
+                            return this.cedula;
+               }
+
+}
